7c0e2f30f763581b5ebbc2145129b0323a3a8203
JesuisNino <74672645+JesuisNino@users.noreply.github.com>
2023-03-20 01:23:36 +0000

    Set up .h file for the Breed C++ function

---
+++ Source/Demo/BreedChildMonster.cpp
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BreedChildMonster.h"
+
+TArray<USkeletalMesh*> UBreedChildMonster::GetChildMeshesArray(const TArray<USkeletalMesh*>& ParentMeshesArray, UDataTable* ChildDataTable)
+{
+	TArray<USkeletalMesh*> ChildMeshesArray;
+
+	
+
+	return ChildMeshesArray;
+}
---
+++ Source/Demo/BreedChildMonster.h
@@ -0,0 +1,122 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataTable.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "BreedChildMonster.generated.h"
+
+/**
+ * 
+ */
+USTRUCT(BlueprintType)
+struct FStruct_MaterialSetup
+{
+	GENERATED_BODY()
+public:
+	/** Please add a variable description */
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (DisplayName = "Paramater Name"))
+		FName ParamaterName;
+
+	/** Please add a variable description */
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (DisplayName = "Slot Index"))
+		int32 SlotIndex;
+
+	/** Please add a variable description */
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (DisplayName = "Material Reference"))
+		TObjectPtr<UMaterial> MaterialReference;
+};
+
+USTRUCT(BlueprintType)
+struct FStruct_MorphTargetSetup
+{
+	GENERATED_BODY()
+public:
+	/** This Text Will Appear On Customization Section */
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (DisplayName = "Widget Name"))
+		FText WidgetName;
+
+	/** Specific Name Of The Morph Target */
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (DisplayName = "Morph Target Name"))
+		FName MorphTargetName;
+
+	/** Please add a variable description */
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (DisplayName = "Default Value"))
+		float DefaultValue;
+
+	/** It Will Convert The X(Min) and Y(Max) Values To -1,1 Range */
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (DisplayName = "Normalize To Range"))
+		FVector2D NormalizeToRange;
+};
+
+USTRUCT(BlueprintType)
+struct FStruct_ColorSchemeSetup
+{
+	GENERATED_BODY()
+public:
+	/** This Text Will Appear On Customization Menu */
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (DisplayName = "Widget Title"))
+		FText WidgetTitle;
+
+	/** Available Colors To Select */
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (DisplayName = "Available Colors"))
+		TArray<FLinearColor> AvailableColors;
+
+	/** Material Reference To Create Dynamic Instance */
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (DisplayName = "Material Setup"))
+		TArray<FStruct_MaterialSetup> MaterialSetup;
+};
+
+USTRUCT(BlueprintType)
+struct FStruct_AvailableMeshMaster
+{
+	GENERATED_BODY()
+public:
+	/** Please add a variable description */
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (DisplayName = "Skeletal Mesh"))
+		TObjectPtr<USkeletalMesh> SkeletalMesh;
+
+	/** Please add a variable description */
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (DisplayName = "Item Icon"))
+		TObjectPtr<UTexture2D> ItemIcon;
+
+	/** Please add a variable description */
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (DisplayName = "Morph Targets"))
+		TArray<FStruct_MorphTargetSetup> MorphTargets;
+
+	/** Please add a variable description */
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (DisplayName = "Color Schemes"))
+		TArray<FStruct_ColorSchemeSetup> ColorSchemes;
+};
+
+
+USTRUCT(BlueprintType)
+struct FStruct_MasterTags
+{
+	GENERATED_BODY()
+public:
+	/** Please add a variable description */
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (DisplayName = "Tab Name"))
+		FText TabName;
+
+	/** Please add a variable description */
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (DisplayName = "Available Meshes"))
+		TArray<FStruct_AvailableMeshMaster> AvailableMeshes;
+
+	/** Please add a variable description */
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (DisplayName = "Rate"))
+		int32 Rate;
+};
+
+
+UCLASS()
+class DEMO_API UBreedChildMonster : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable, Category = "Breed Child Monster")
+	static TArray<USkeletalMesh*> GetChildMeshesArray(const TArray<USkeletalMesh*>& ParentMeshesArray, UDataTable* ChildDataTable);
+	
+};
